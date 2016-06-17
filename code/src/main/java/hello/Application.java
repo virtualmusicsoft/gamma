2c4b4320883d03c946f0dd21c6dfef5c81b95f16
Daniel <danielmenezes@gmail.com>
2016-06-17 14:28:32 -0300

    verify jre 32bits

--- code/src/main/java/hello/Application.java
+++ code/src/main/java/hello/Application.java
@@ -9,7 +9,20 @@ import jportmidi.JPortMidiException;
 public class Application {
 	
     public static void main(String[] args) throws JPortMidiException {
+    	checkJava32bits();
         SpringApplication.run(Application.class, args);
+    }
+    
+    private static void checkEnveriment() {
+    	checkJava32bits();
+    }
+    
+    private static void checkJava32bits() {
+    	if (!"32".equals(System.getProperty("sun.arch.data.model"))) {
+    		//TODO: internationalization
+    		System.out.println("You need a Java 32 bits!");
+    		System.exit(-1);
+    	}
     }
 
 }
---
+++ code/startServer.bat
@@ -0,0 +1,2 @@
+cp ..\pmjni.dll .
+java -jar gmnt-server-0.1.0.jar
